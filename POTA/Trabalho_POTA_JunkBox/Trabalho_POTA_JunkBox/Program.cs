d99d891bc2156a1c17df9c99763b11ef11f1f25b
kiyohider <56371934+kiyohider@users.noreply.github.com>
2021-04-22 09:48:48 -0300

    begin
    create project

---
+++ POTA/Trabalho_POTA_JunkBox/Trabalho_POTA_JunkBox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trabalho_POTA_JunkBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
